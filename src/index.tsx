12817428c99b27db011747950b7b25413b50f5c9
Igor Korobyev <iKorobyev@gmail.com>
2021-10-30 19:45:45 +0300

    Add redux store, hooks, react-router

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
+import { setupStore } from "./store/store";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = setupStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,13 @@
-import { combineReducers } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
 const rootReducer = combineReducers({});
 
-// export const setupStore = () => {
-//   return
-// }
+export const setupStore = () => {
+  return configureStore({
+    reducer: rootReducer,
+  });
+};
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore["dispatch"];
